37e49eb161c5acf733f5d939eac7de58b1a0e53d
Ivan Sytnik <ivan.sytnik02@gmail.com>
2022-10-29 17:35:47 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,12 +9,20 @@ player.on('timeupdate',
     player.getCurrentTime().then(data => localStorage.setItem("videoplayer-current-time", data))
   }, 1000)
 );
-
+ 
  let timePlayer = localStorage.getItem("videoplayer-current-time");
 
- console.log(`Загруженное време с локального хранилища:${timePlayer}`);
-
-player.setCurrentTime(timePlayer).then(function() {
+ console.log(`Загруженное време с локального хранилища:${localTime()}`);
+ function localTime() {
+  if(timePlayer == null) {
+    return 0;
+   }
+   
+    return timePlayer;
+ }
+ 
+player.setCurrentTime(localTime()).then(function() {
      // seconds = the actual time that the player seeked to
+ 
  });
 
